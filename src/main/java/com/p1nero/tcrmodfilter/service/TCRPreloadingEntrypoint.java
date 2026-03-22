f2e843362f78a6ac2fa89b57c8284213079714a2
p1nero <1070402601@qq.com>
2026-03-23 07:12:53 +0800

    移除加速碰撞

--- src/main/java/com/p1nero/tcrmodfilter/service/TCRPreloadingEntrypoint.java
+++ src/main/java/com/p1nero/tcrmodfilter/service/TCRPreloadingEntrypoint.java
@@ -2,6 +2,7 @@ package com.p1nero.tcrmodfilter.service;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.fml.loading.ImmediateWindowHandler;
+import org.apache.commons.lang3.SystemUtils;
 import org.slf4j.Logger;
 import settingdust.preloading_tricks.api.PreloadingEntrypoint;
 import settingdust.preloading_tricks.api.PreloadingTricksCallbacks;
@@ -9,6 +10,10 @@ import settingdust.preloading_tricks.api.PreloadingTricksCallbacks;
 import java.util.Set;
 
 public class TCRPreloadingEntrypoint implements PreloadingEntrypoint {
+
+    private static boolean acceleratedRenderingRemoved;
+    private static boolean yesSteveModelRemoved;
+    private static boolean acceleratedRecollidingRemoved;
 
     public static final Logger LOGGER = LogUtils.getLogger();
 
@@ -23,15 +28,38 @@ public class TCRPreloadingEntrypoint implements PreloadingEntrypoint {
             if(major < 4 || (major == 4 && minor < 6)) {
                 if(modManager.removeById("acceleratedrendering")){
                     LOGGER.warn("GLVersion < 4.6, acceleratedrendering mod has been removed.");
+                    acceleratedRenderingRemoved = true;
                 }
             }
-            if(modManager.getById("epicfight") != null && modManager.removeById("yes_steve_model")){
-                LOGGER.warn("Incompatible with epic fight, yes_steve_model mod has been removed.");
+            String arch = System.getProperty("sun.arch.data.model");
+            if((SystemUtils.IS_OS_MAC || !arch.equals("64"))) {
+                if(modManager.removeById("acceleratedrecoiling")) {
+                    LOGGER.warn("On MacOS or 32-bit arch, acceleratedrecoiling mod has been removed.");
+                    acceleratedRecollidingRemoved = true;
+                }
+            }
+            if(modManager.getById("epicfight") != null){
+                if(modManager.removeById("yes_steve_model")) {
+                    LOGGER.warn("Incompatible with epic fight, yes_steve_model mod has been removed.");
+                    yesSteveModelRemoved = true;
+                }
             }
             if(modManager.getById("tcrcore") != null) {
                 modManager.removeByIds(Set.of("projecte"));
             }
         });
+    }
+
+    public static boolean isAcceleratedRecollidingRemoved() {
+        return acceleratedRecollidingRemoved;
+    }
+
+    public static boolean isAcceleratedRenderingRemoved() {
+        return acceleratedRenderingRemoved;
+    }
+
+    public static boolean isYesSteveModelRemoved() {
+        return yesSteveModelRemoved;
     }
 
 }
